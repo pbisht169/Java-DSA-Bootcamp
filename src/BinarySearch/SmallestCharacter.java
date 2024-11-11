fe6230683aadbcbae6a76bdc2be19050f0ec4b16
pbisht17 <pawanbisht94.pb@gmail.com>
2024-11-11 16:43:37 +0530

    Comments added

--- src/BinarySearch/SmallestCharacter.java
+++ src/BinarySearch/SmallestCharacter.java
@@ -1,7 +1,11 @@
 package BinarySearch;
 /**
- * In this we need to find out the cieling of a character in an array but the result should be in wrap around. That is if there is no character in the
+ * In this we need to find out the ceiling of a character in an array but the result should be in wrap around. That is if there is no character in the
  * given array then will provide the first character
+ * Exactly same approach to find a ceiling of a number
+ * Ignore the condition equal to as we're looking for the greater element
+ * Condition violated as Start = End + 1, means our start at the desired location, means smallest character which is greater than the target element.
+ * therefore we return the Start % Array_Size as we need the wrap around character if there is no character found
  * @returns
  */
 public class SmallestCharacter {
